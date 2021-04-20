8454adf0e74e290b5ea9bca1ee45cac37b96392f
siddshrestha <45787147+siddshrestha@users.noreply.github.com>
2021-04-20 08:09:41 +0545

    v-for key attribute

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,12 +1,31 @@
 <template>
   <div id="movie-container">
-      movie list
+      <ul>
+          <li style="color: white" v-for="(movie, index) in movies" :key="movie.id">{{index}}:{{movie.name}}</li>
+      </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            movies: [
+                {
+                    id: 1,
+                    name: 'Shawshank Redemption'
+                },
+                {
+                    id: 2,
+                    name: 'Seven'
+                },
+                {
+                    id: 3,
+                    name: 'Pagglait'
+                }
+            ]
+        }
+    },
 }
 </script>
 
